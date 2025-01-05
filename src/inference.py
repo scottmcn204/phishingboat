c8efcbbe0bfb510bf197db33dba3b799dbe942ce
scottmcn204 <mcnallsc@tcd.ie>
2025-01-05 17:59:44 +0100

    readable inference results

--- src/inference.py
+++ src/inference.py
@@ -9,25 +9,25 @@ def run_inference(features) :
     input_name = session.get_inputs()[0].name
     result = session.run(None, {input_name: input_data})
     prediction = result[0][0] 
-    print("Predicted Output:", prediction)
+    if(prediction == 1):
+        print("We believe website is Phishing D:")
+    else:
+        print("We believe website is safe :)")
 
 # Load the ONNX model
 session = rt.InferenceSession("models/model.onnx")
+while(True):
+    url = input("Enter website url: ")
+    response = requests.get(url)
+    if response.status_code == 200:
+        html_content = response.text
+        html_features = parsing.html_features_from_text(html_content)
+        url_features = parsing.url_features_inference(url)
+        features = url_features + html_features
+        print("HTML Features:", html_features)
+        print("URL Features:", url_features)
+        print("Combined Features:", features)
+        run_inference(features)
+    else:
+        print(f"Couldn't fetch page, Status : {response.status_code} :(")
 
-url = input("Enter website url: ")
-response = requests.get(url)
-if response.status_code == 200:
-    html_content = response.text
-    html_features = parsing.html_features_from_text(html_content)
-    url_features = parsing.url_features_inference(url)
-    features = url_features + html_features
-    print("HTML Features:", html_features)
-    print("URL Features:", url_features)
-    print("Combined Features:", features)
-    run_inference(features)
-else:
-    print(f"Couldn't fetch page, Status : {response.status_code} :(")
-
-
-# url_features = parsing.url_features(url)
-# html_features = parsing.hmtl_features(path_to_html)
